0bca33fbea6fbb81b9a424b64027d480ab28ce1c
ASSIAKH <44146507+ASSIAKH@users.noreply.github.com>
2019-01-21 12:06:11 +0100

    c

--- tp6/meteo-angular/src/app/app.module.ts
+++ tp6/meteo-angular/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MeteoComponent } from './meteo/meteo.component';
+import { RouterModule, Routes } from '@angular/router';
 
 @NgModule({
   declarations: [
